cc13829ba374e930274ace29bb1df66442b1213d
omkar shelke <omkar1144@gmail.com>
2025-07-07 18:36:48 +0530

    redirects added

--- frontend/src/components/MemberTable/MemberTable.jsx
+++ frontend/src/components/MemberTable/MemberTable.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import TableBody from './TableBody'
+import EditMember from '../EditMember/EditMember'
 import axios from 'axios'
 
 const MemberTable = () => {
