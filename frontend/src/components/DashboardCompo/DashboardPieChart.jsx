dc09c6f85dff2e5f70ed2aed836c9b11d8e10a1a
omkar shelke <omkar1144@gmail.com>
2025-07-07 20:00:03 +0530

    dashboard

--- frontend/src/components/DashboardCompo/DashboardPieChart.jsx
+++ frontend/src/components/DashboardCompo/DashboardPieChart.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Pie } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
 import axios from 'axios';
 
 const DashboardPieChart = () => {
